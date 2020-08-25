52432acb3927a9ce9a7862604d7a90503893e308
JHolford <62012303+JHolford@users.noreply.github.com>
2020-08-25 11:59:14 +0100

    Update hometab.php

--- hometab.php
+++ hometab.php
@@ -1,5 +1,8 @@
 <?php
 
-echo ("Please use the tabs above to navigate!")
+echo ("<h4>Please use the tabs above to navigate!</h4>");
+echo ("<br />");
+echo ("*Please note that all values are estimates based on average UK consumer hardware and do not necessarily reflect your actual CO2 Emission savings*")
+
 
 ?>
