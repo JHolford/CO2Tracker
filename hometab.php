7c35c216f24d611a09eb252aaf80f34749e363bc
JHolford <62012303+JHolford@users.noreply.github.com>
2020-08-31 14:41:35 +0100

    Update hometab.php

--- hometab.php
+++ hometab.php
@@ -2,7 +2,9 @@
 
 echo ("<h4>Please use the tabs above to navigate!</h4>");
 echo ("<br />");
-echo ("*Please note that all values are estimates based on average UK consumer hardware and do not necessarily reflect your actual CO2 Emission savings*")
+echo ("<h5>By reducing your CO2 Emissions, you can help to slow climate change AND lower your energy bill!</h5>");
+echo ("<br />");
+echo ("*Please note that all values are estimates based on average UK consumer hardware (As provided by CarbonKit who collate data from the UK National Grid) and do not necessarily reflect your actual CO2 Emission savings*")
 
 
 ?>
